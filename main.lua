e3fbaf3e07e3f76f2d8a80cf95112a978cbfecd3
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-01-18 14:39:30 +0800

    vignette is influenced by ship speed

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ intro:init()
 
 love.graphics.setBackgroundColour(intro.hex("152634"))
 
-local player = {x=love.graphics.getWidth()/2, y=love.graphics.getHeight()/2, xV=0, yV=0, dir = 1, dirV = 0, fuel = 100}
+local player = {x=love.graphics.getWidth()/2, y=love.graphics.getHeight()/2, xV=0, yV=0, dir = 0, dirV = 0, fuel = 100}
 player.img = love.graphics.newImage("spaceship.png")
 
 local particles = love.graphics.newParticleSystem(love.graphics.newImage("particle.png"), 3200)
@@ -12,7 +12,7 @@ particles:setSpeed(400, 700)
 particles:setColors({1, 1, 0}, {1, 0.3, 0}, {0.5, 0.5, 0.5, 0})
 particles:setSizes(0.2, 1)
 
-local vignette = love.graphics.newImage("vignette.png")
+local vignette = {img = love.graphics.newImage("vignette.png"), intensity = 1}
 
 function love.update(dt)
 	intro:update(dt)
@@ -29,6 +29,8 @@ function love.update(dt)
 		for i=1, 8 do
 			particles:moveTo(player.x - math.sin(player.dir)*20*i/30, player.y + math.cos(player.dir)*20*i/30)
 			particles:emit(1, player.x, player.y)
+
+			vignette.intensity = math.min(vignette.intensity + 0.01, 1)
 		end
 
 		player.fuel = player.fuel - 1
@@ -56,17 +58,19 @@ function love.update(dt)
 	if player.fuel >= 0.05 then 
 		player.dirV = player.dirV * 0.9 
 	end
+
+	if intro.phase > 1 then vignette.intensity = vignette.intensity + (0.45 - vignette.intensity)*0.1 end
 end
 
 function love.draw()
-	-- love.graphics.push()
-	-- love.graphics.translate(-player.x+love.graphics.getWidth()/2, -player.y+love.graphics.getHeight()/2)
+	love.graphics.push()
+	love.graphics.translate(-player.x+love.graphics.getWidth()/2 - player.xV*2, -player.y+love.graphics.getHeight()/2 - player.yV*2)
 	love.graphics.draw(particles)
 	love.graphics.draw(player.img, player.x, player.y, player.dir, 0.4, nil, player.img:getWidth()/2, player.img:getHeight()/2)
-	-- love.graphics.pop()
+	love.graphics.pop()
 
-	love.graphics.setColour(1,1,1,0.5)
-	love.graphics.draw(vignette, 0,0, nil, love.graphics.getWidth()/vignette:getWidth(), love.graphics.getHeight()/vignette:getHeight())
+	love.graphics.setColour(1,1,1,vignette.intensity)
+	love.graphics.draw(vignette.img, 0,0, nil, love.graphics.getWidth()/vignette.img:getWidth(), love.graphics.getHeight()/vignette.img:getHeight())
 
 	-- intro.pprint(player.fuel)
 	love.graphics.setColour(1,1,1)
@@ -75,5 +79,14 @@ function love.draw()
 	love.graphics.rectangle("fill", 10+padding*2, 10+padding*2, (150-padding*4)/100*player.fuel, 20-padding*4)
 	love.graphics.rectangle("line", 10, 10, 150, 20)
 
+	-- intro.pprint(vignette.intensity, 100)
+
 	intro:draw()
+end
+
+function love.keypressed(k)
+	if k == "r" then
+		player = {x=love.graphics.getWidth()/2, y=love.graphics.getHeight()/2, xV=0, yV=0, dir = 0, dirV = 0, fuel = 100}
+		player.img = love.graphics.newImage("spaceship.png")
+	end	
 end
